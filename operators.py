b0314ffd3a3450c40f738c3a92c720919aefed55
Spoorthi <spoorthisunitha589@gmail.com>
2026-02-25 12:48:22 +0530

    worked various operators in python

---
+++ operators.py
@@ -0,0 +1,36 @@
+
+
+a=int(input("enter a number:"))
+b=int(input("enter a number"))
+
+
+if(a > 10 and b>10):
+    print("greater")
+
+if(a<5 or b<5):
+    print("yes lesser")
+
+if not(a>b):
+    print("yes not greater")
+
+
+str="Spoorthi"
+print("Spoorthi" in str)
+
+my_list=["spoo","sne","2",4]
+print("2" in my_list)
+
+
+a=10
+b=2
+c= a&b
+print(c)
+
+print(a>>2)  
+
+print(a<<2) 
+
+a=33
+b=20
+print(a>>2)
+print(a//b)
