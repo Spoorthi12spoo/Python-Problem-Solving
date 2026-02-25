09eb9142ce4caf6e7661e42a4d8081020fd27790
Spoorthi <spoorthisunitha589@gmail.com>
2026-02-25 12:52:45 +0530

    worked on various operators in python

--- operators.py
+++ operators.py
@@ -1,9 +1,6 @@
-
-
+#> and <
 a=int(input("enter a number:"))
 b=int(input("enter a number"))
-
-
 if(a > 10 and b>10):
     print("greater")
 
@@ -13,24 +10,25 @@ if(a<5 or b<5):
 if not(a>b):
     print("yes not greater")
 
-
+#in
 str="Spoorthi"
 print("Spoorthi" in str)
 
 my_list=["spoo","sne","2",4]
 print("2" in my_list)
 
-
+#&
 a=10
 b=2
 c= a&b
 print(c)
 
-print(a>>2)  
-
-print(a<<2) 
+#left shift and right shift
 
 a=33
 b=20
+print(a<<2) 
 print(a>>2)
+
+#floor
 print(a//b)
