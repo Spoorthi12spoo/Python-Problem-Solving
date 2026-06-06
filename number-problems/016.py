463f906943e8970a5a11d0a37056a7bd0e86891b
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-06 11:44:41 +0530

    Add program to calculate square root of a number
    Add a Python program to calculate the square root of a number using both exponentiation and the math module.

---
+++ number-problems/016.py
@@ -0,0 +1,11 @@
+#016.Python Program to calculate the square root of a given number.
+
+num=int(input())
+sqrt=num**0.5  
+print(sqrt)
+
+#or
+import math
+num=int(input())
+square_root=math.sqrt(num)
+print(square_root)
