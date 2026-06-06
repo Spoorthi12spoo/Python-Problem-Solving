3d1c044c8bfa25bfca854d1f79fc78482bc4295e
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-06 11:34:21 +0530

    Change problem identifier from #8 to #005
    Updated problem identifier in the comment.

---
+++ number-problems/005.py
@@ -0,0 +1,9 @@
+#005.	Write a program in Python to find greatest among three integers.
+
+a,b,c=map(int,input("enter three numbers: ").split())
+if (a>b) and (a>c):
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
