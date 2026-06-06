6ba159a9168d96d1305635033f743128bf657c49
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-06 11:39:18 +0530

    Add Python program to swap two numbers

---
+++ number-problems/009.py
@@ -0,0 +1,8 @@
+#009.Write a program in Python to swap two numbers using third variable
+
+a=int(input("enter a number: "))
+b=int(input("enter a number: "))
+temp=a
+a=b
+b=temp
+print("a=",a ,"b= " ,b)
