7599763a19e61a1a3bf152137878558f92829609
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-07 12:01:25 +0530

    Implement subset check for two sets
    Added a script to check subset relationships between sets.

---
+++ set-problems/010.py
@@ -0,0 +1,8 @@
+#010.Check if one set is a subset of another.
+
+A = {1, 2}
+B = {1, 2, 3, 4}
+
+print(A.issubset(B))   #subset
+print(A<=B)            #subset
+print(A<B)             #proper subset
