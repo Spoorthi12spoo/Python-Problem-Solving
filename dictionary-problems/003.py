422f00a6a53752148b46129c4db459424c8ec5b9
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-08 19:38:04 +0530

    Add key-value pairs to college dictionary
    Added a new key-value pair 'c_name' and updated 'location' in the college dictionary.

---
+++ dictionary-problems/003.py
@@ -0,0 +1,7 @@
+#003. Add a new key–value pair to an existing dictionary.
+
+college={"princi":"abcd","student":1000,"faculty":100}
+college["c_name"]="xyz";
+college.update({"location":"mysuru"})  # update(location="mysuru")
+print(college)
+
