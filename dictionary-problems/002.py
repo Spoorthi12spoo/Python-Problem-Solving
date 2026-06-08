f0d133e91a5d1917885d99ab6e7dea9acbd905c1
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-08 19:37:11 +0530

    Add key existence check in student dictionary
    Implement key existence check in dictionary.

---
+++ dictionary-problems/002.py
@@ -0,0 +1,8 @@
+#002. Write a program to check if a given key exists in a dictionary.
+
+student={"name":"abc","branch":"CSE","year":2}
+key="name"
+if key in student:
+    print("key exists")
+else:
+    print("key not exists")
