87b5d48e587d8934848568f2d1a6fbffa4448f68
Spoorthi <spoorthisunitha589@gmail.com>
2026-06-08 20:42:57 +0530

    Implement additional dictionary filtering methods
    Added alternative methods to filter out None values from a dictionary.

--- dictionary-problems/026.py
+++ dictionary-problems/026.py
@@ -7,3 +7,17 @@ for i in D:
     if D[i] is not None:   #   if D[i]!=None:
         D1[i]=D[i]
 print(D1)
+
+#another way
+D={'a': 1, 'b': None, 'c': 3} 
+D2={}
+for key,value in D.items():
+    if value is not None:
+        D2[key]=value
+print(D2)
+D={'a': 1, 'b': None, 'c': 3} 
+D2={}
+for key,value in D.items():
+    if key is not None:
+        D2[key]=value
+print(D2)
